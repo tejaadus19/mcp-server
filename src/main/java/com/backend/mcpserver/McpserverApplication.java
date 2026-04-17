cbf32c222cfdeadfe875c7cbc3d1f868e6e6c841
suryatejaadusumalli99@gmail.com <suryatejaadusumalli99@gmail.com>
2026-04-17 10:07:23 -0400

    initial commit of Mcp-Server

---
+++ src/main/java/com/backend/mcpserver/McpserverApplication.java
@@ -0,0 +1,13 @@
+package com.backend.mcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/mcpserver/McpserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
